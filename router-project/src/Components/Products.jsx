f2789da5e96981721fe4106ed6c19c1555fde894
tarunaNangla <tarunanangla131@gmail.com>
2022-06-02 21:30:14 +0530

    done

--- router-project/src/Components/Products.jsx
+++ router-project/src/Components/Products.jsx
@@ -4,23 +4,42 @@ import { useState } from 'react';
 import axios from "axios"
 
 const Products = () => {
+
     const [data,setData]=useState([]);
     const [loading,setLoading]=useState(false);
-    useEffect (()=>{
-      axios({
-        url:"  http://localhost:2022/data",
-        method:"get"
-      }).then(res=>{
-        setData(res.data);
-  
-      }).catch(error=>{
-        setLoading(false)
-      })
+    useEffect(()=>{
+        setLoading(true)
+        axios({
+            url:"http://localhost:3000/data",
+            method:"get"
+        }).then(res=>{
+            setData(res.data);
+
+        }).catch(err=>{
+            setLoading(false)
+        })
     },[])
     console.log(data)
+   
   return (
     <div>
-      <h1>11</h1>
+      <h1>Products</h1>
+      <div className='grid'>
+
+      {
+          data.map((ele)=>{
+              return(
+                  <div key={ele.id}>
+                      <h4>{ele.category}</h4>
+                      {/* <p>{ele.description}</p> */}
+                      <img style={{width:"200px",height:"200px"}} src={ele.image} alt="product" /> 
+                      <h3>{ele.price}</h3>
+                      <p>{ele.title}</p>
+                  </div>
+              )
+          })
+      }
+      </div>
     </div>
   )
 }
